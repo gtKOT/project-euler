ace9717bd746fb93645f8bcad2b12bec7cf91c77
KOT <askastr@gmail.com>
2012-07-15 20:21:14 +0900

    checkTriple -> checkDigOverlap

--- src/problem32.py
+++ src/problem32.py
@@ -8,40 +8,40 @@ def check(num, already):
         else:
             already.append(dig)
 
-    return True;
+    return True
 
 
-def checkTriple(num1, num2, num3):
+def checkDigOverlap(num1, num2, num3):
     already = ["0"]
 
     return check(num1, already) and check(num2, already) and check(num3, already)
 
 
 
-suitable = set();
+suitable = set()
 
 # check [1dig * 4dig = 4dig]
 for oneDig in range(1, 10):
     for fourDig in range(1000, 10000):
-        right = oneDig * fourDig
+        rhs = oneDig * fourDig
 
-        if right >= 10000:
+        if rhs >= 10000:
             break
 
-        if checkTriple(oneDig, fourDig, right):
-            suitable.add(right)
-            print [oneDig, fourDig, right]
+        if checkDigOverlap(oneDig, fourDig, rhs):
+            suitable.add(rhs)
+            print [oneDig, fourDig, rhs]
 
 # check [2dig * 3dig = 4dig]
 for twoDig in range(10, 100):
     for threeDig in range(100, 1000):
-        right = twoDig * threeDig
+        rhs = twoDig * threeDig
 
-        if right >= 10000:
+        if rhs >= 10000:
             break
 
-        if checkTriple(twoDig, threeDig, right):
-            suitable.add(right)
-            print [twoDig, threeDig, right]
+        if checkDigOverlap(twoDig, threeDig, rhs):
+            suitable.add(rhs)
+            print [twoDig, threeDig, rhs]
 
 print sum(suitable)
